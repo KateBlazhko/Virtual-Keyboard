d6f77e8980ae81f308f15e496368c75a5d1df173
KateBlazhko <kateugnicheva@gmail.com>
2022-04-08 09:55:16 +0300

    fix: change links

--- shelter/src/main/index.js
+++ shelter/src/main/index.js
@@ -1,4 +1,8 @@
 import mainStyle from './style.scss';
-import {sliderMain} from '../assets/js/page-element.js';
+import {Slider} from '../assets/js/page-element.js';
+
+
+const contentSlider = document.querySelector('.pets-content-slider');
+const sliderMain = new Slider(contentSlider, 'pets-content-slider-main');
 
 sliderMain.createCards(3);
