4a2f0fcc3dfc9cc37f948938f3d80ba59d528b2c
KateBlazhko <kateugnicheva@gmail.com>
2022-04-15 12:24:55 +0300

    fix: change burger

--- shelter/src/assets/js/burger.js
+++ shelter/src/assets/js/burger.js
@@ -15,8 +15,8 @@ function toggleMenu() {
       return false;
     };
   
-    toggleClassName('active', menuToggle, menu);
-    toggleClassName('lock', body);
+    toggleClassName('active', menuToggle, menu, logo);
+    toggleClassName('lock', body, logoMain);
     const bodyLock = document.querySelector('.lock'); 
     bodyLock && bodyLock.addEventListener('click', closeMenu);
 }
@@ -25,16 +25,17 @@ function toggleMenu() {
 function closeMenu(event) {
     if (event.target.classList.contains('nav-link') ||
        (event.target.classList.contains('lock'))) {
-        removeClassName('active', menuToggle, menu);
-        removeClassName('lock', body);
+        removeClassName('active', menuToggle, menu, logo);
+        removeClassName('lock', body, logoMain);
     }
 }
 
 
 // burger-menu
 const body = document.body;
-
 const menuToggle = document.querySelector('.menu-toggle');
+const logo = document.querySelector('.double');
+const logoMain = document.querySelector('.main');
 const menu = document.querySelector('.nav');
 menuToggle.addEventListener('click', toggleMenu);
 menu.addEventListener('click', closeMenu);
